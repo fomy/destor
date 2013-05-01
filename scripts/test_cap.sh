c28ecb2cf08104e85f770bf52c976b2f0540e7fe
fomy <fumin@hust.edu.cn>
2013-05-01 21:13:16 +0800

    A few modifications in scripts and ChangeLog.

--- scripts/test_cap.sh
+++ scripts/test_cap.sh
@@ -4,7 +4,7 @@ rewrite=CAP
 
 for cache_size in 32 28 24 20 16 12 10 8 6 4 2 0
 do
-for param in 26 22 18 14 12 10 8 6 
+for param in 20 18 16 14 12 10 8 6 
 do
 ./test_rewrite_linux.sh $rewrite $param $cache_size
 done
@@ -12,7 +12,7 @@ done
 
 for cache_size in 512 384 256 192 128 96 64 32 16 8 0 
 do
-for param in 26 22 18 14 12 10 8 6
+for param in 20 18 16 14 12 10 8 6 
 do
 ./test_rewrite_vmdk.sh $rewrite $param $cache_size
 done
