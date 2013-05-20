c6cd3f589b5de49f1aae4886d3fb1e0e22c09c3f
fomy <fumin@hust.edu.cn>
2013-05-20 21:53:20 +0800

    Fix a bug causing segmentation fault in load_historical_sparse_containers().

--- src/storage/container_usage_monitor.c
+++ src/storage/container_usage_monitor.c
@@ -9,49 +9,51 @@
 
 extern char working_path[];
 
-extern double hbr_usage_threshold; 
+extern double hbr_usage_threshold;
 
-static ContainerUsage* container_usage_new(ContainerId cntnr_id){
-    ContainerUsage* cntnr_usg = (ContainerUsage*)malloc(sizeof(ContainerUsage));
-    cntnr_usg->cntnr_id = cntnr_id;
-    cntnr_usg->read_size = CONTAINER_DES_SIZE;
-    return cntnr_usg;
+static ContainerUsage* container_usage_new(ContainerId cntnr_id) {
+	ContainerUsage* cntnr_usg = (ContainerUsage*) malloc(
+			sizeof(ContainerUsage));
+	cntnr_usg->cntnr_id = cntnr_id;
+	cntnr_usg->read_size = CONTAINER_DES_SIZE;
+	return cntnr_usg;
 }
 
-static void container_usage_free(ContainerUsage* cntnr_usg){
-    free(cntnr_usg);
+static void container_usage_free(ContainerUsage* cntnr_usg) {
+	free(cntnr_usg);
 }
 
-static double container_usage_calc(ContainerUsage* cntnr_usg){
-    return cntnr_usg->read_size/(double)CONTAINER_SIZE;
+static double container_usage_calc(ContainerUsage* cntnr_usg) {
+	return cntnr_usg->read_size / (double) CONTAINER_SIZE;
 }
 
-static double container_usage_add(ContainerUsage* cntnr_usg, Fingerprint* fngrprnt, int32_t chnk_lngth){
-    cntnr_usg->read_size += chnk_lngth+CONTAINER_META_ENTRY_SIZE;
-    return container_usage_calc(cntnr_usg);
+static double container_usage_add(ContainerUsage* cntnr_usg,
+		Fingerprint* fngrprnt, int32_t chnk_lngth) {
+	cntnr_usg->read_size += chnk_lngth + CONTAINER_META_ENTRY_SIZE;
+	return container_usage_calc(cntnr_usg);
 }
 
 /*
  * monitor interfaces:
  * new, free, update
  */
-ContainerUsageMonitor* container_usage_monitor_new(){
-    ContainerUsageMonitor* cntnr_usg_mntr 
-        = (ContainerUsageMonitor*)malloc(sizeof(ContainerUsageMonitor));
+ContainerUsageMonitor* container_usage_monitor_new() {
+	ContainerUsageMonitor* cntnr_usg_mntr = (ContainerUsageMonitor*) malloc(
+			sizeof(ContainerUsageMonitor));
 
-    cntnr_usg_mntr->dense_map = 
-        g_hash_table_new_full(g_int_hash, g_int_equal, NULL, container_usage_free);
-    cntnr_usg_mntr->sparse_map 
-        = g_hash_table_new_full(g_int_hash, g_int_equal, NULL, container_usage_free);
+	cntnr_usg_mntr->dense_map = g_hash_table_new_full(g_int_hash, g_int_equal,
+			NULL, container_usage_free);
+	cntnr_usg_mntr->sparse_map = g_hash_table_new_full(g_int_hash, g_int_equal,
+			NULL, container_usage_free);
 
-    return cntnr_usg_mntr;
+	return cntnr_usg_mntr;
 }
 
-void container_usage_monitor_free(ContainerUsageMonitor* cntnr_usg_mntr){
-    g_hash_table_destroy(cntnr_usg_mntr->dense_map);
-    g_hash_table_destroy(cntnr_usg_mntr->sparse_map);
+void container_usage_monitor_free(ContainerUsageMonitor* cntnr_usg_mntr) {
+	g_hash_table_destroy(cntnr_usg_mntr->dense_map);
+	g_hash_table_destroy(cntnr_usg_mntr->sparse_map);
 
-    free(cntnr_usg_mntr);
+	free(cntnr_usg_mntr);
 }
 
 /* --------------------------------------------------------------------------*/
@@ -71,23 +73,27 @@ void container_usage_monitor_free(ContainerUsageMonitor* cntnr_usg_mntr){
  * @Param len, length of the chunk
  */
 /* ----------------------------------------------------------------------------*/
-void container_usage_monitor_update(ContainerUsageMonitor* cntnr_usg_mntr, 
-        ContainerId cntnr_id, Fingerprint *fngr, int32_t len){
-    ContainerUsage* cntnr_usg = g_hash_table_lookup(cntnr_usg_mntr->dense_map, &cntnr_id);
-    if(cntnr_usg){
-        container_usage_add(cntnr_usg, NULL, len);
-    }else{
-        ContainerUsage *cntnr_usg = g_hash_table_lookup(cntnr_usg_mntr->sparse_map, &cntnr_id);
-        if(!cntnr_usg){
-            cntnr_usg = container_usage_new(cntnr_id);
-            g_hash_table_insert(cntnr_usg_mntr->sparse_map, &cntnr_usg->cntnr_id, cntnr_usg);
-        }
-        double usage = container_usage_add(cntnr_usg, fngr, len);
-        if(usage > hbr_usage_threshold){
-            g_hash_table_steal(cntnr_usg_mntr->sparse_map, &cntnr_id);
-            g_hash_table_insert(cntnr_usg_mntr->dense_map, &cntnr_usg->cntnr_id, cntnr_usg);
-        }
-    }
+void container_usage_monitor_update(ContainerUsageMonitor* cntnr_usg_mntr,
+		ContainerId cntnr_id, Fingerprint *fngr, int32_t len) {
+	ContainerUsage* cntnr_usg = g_hash_table_lookup(cntnr_usg_mntr->dense_map,
+			&cntnr_id);
+	if (cntnr_usg) {
+		container_usage_add(cntnr_usg, NULL, len);
+	} else {
+		ContainerUsage *cntnr_usg = g_hash_table_lookup(
+				cntnr_usg_mntr->sparse_map, &cntnr_id);
+		if (!cntnr_usg) {
+			cntnr_usg = container_usage_new(cntnr_id);
+			g_hash_table_insert(cntnr_usg_mntr->sparse_map,
+					&cntnr_usg->cntnr_id, cntnr_usg);
+		}
+		double usage = container_usage_add(cntnr_usg, fngr, len);
+		if (usage > hbr_usage_threshold) {
+			g_hash_table_steal(cntnr_usg_mntr->sparse_map, &cntnr_id);
+			g_hash_table_insert(cntnr_usg_mntr->dense_map, &cntnr_usg->cntnr_id,
+					cntnr_usg);
+		}
+	}
 }
 
 /* --------------------------------------------------------------------------*/
@@ -98,65 +104,71 @@ void container_usage_monitor_update(ContainerUsageMonitor* cntnr_usg_mntr,
  * @Param job_id
  */
 /* ----------------------------------------------------------------------------*/
-int container_usage_monitor_print(ContainerUsageMonitor *cntnr_usg_mntr, int32_t job_id,
-        GHashTable* old_sparse_containers){
-    char path[500];
-    strcpy(path, working_path);
-    strcat(path, "jobs/");
-    char file_name[20];
-    sprintf(file_name, "job%d.sparse", job_id);
-    strcat(path, file_name);
-    FILE* usage_file = fopen(path, "w+");
-    GHashTableIter iter;
-    gpointer key, value;
-    /* sparse containers */
-    int old_sparse_num = 0;
-    g_hash_table_iter_init(&iter, cntnr_usg_mntr->sparse_map);
-    while(g_hash_table_iter_next(&iter, &key, &value)){
-        ContainerUsage* cntnr_usg = (ContainerUsage*)value;
-        if(old_sparse_containers && g_hash_table_lookup(old_sparse_containers, &cntnr_usg->cntnr_id)){
-            old_sparse_num++;
-        }
-        fprintf(usage_file, "%d:%d:%.2f\n", cntnr_usg->cntnr_id, cntnr_usg->read_size, 
-                container_usage_calc(cntnr_usg));
-    }
-    if(fclose(usage_file)<0){
-        /* Error may happen here. */
-        perror(strerror(errno));
-        return -1;
-    }
-    return old_sparse_num;
+int container_usage_monitor_print(ContainerUsageMonitor *cntnr_usg_mntr,
+		int32_t job_id, GHashTable* old_sparse_containers) {
+	char path[500];
+	strcpy(path, working_path);
+	strcat(path, "jobs/");
+	char file_name[20];
+	sprintf(file_name, "job%d.sparse", job_id);
+	strcat(path, file_name);
+	FILE* usage_file = fopen(path, "w+");
+	GHashTableIter iter;
+	gpointer key, value;
+	/* sparse containers */
+	int old_sparse_num = 0;
+	g_hash_table_iter_init(&iter, cntnr_usg_mntr->sparse_map);
+	while (g_hash_table_iter_next(&iter, &key, &value)) {
+		ContainerUsage* cntnr_usg = (ContainerUsage*) value;
+		if (old_sparse_containers
+				&& g_hash_table_lookup(old_sparse_containers,
+						&cntnr_usg->cntnr_id)) {
+			old_sparse_num++;
+		}
+		fprintf(usage_file, "%d:%d:%.2f\n", cntnr_usg->cntnr_id,
+				cntnr_usg->read_size, container_usage_calc(cntnr_usg));
+	}
+	if (fclose(usage_file) < 0) {
+		/* Error may happen here. */
+		perror(strerror(errno));
+		return -1;
+	}
+	return old_sparse_num;
 }
 
-GHashTable* load_historical_sparse_containers(int32_t job_id){
-    if(job_id == 0){
-        return NULL;
-    }
-    char path[500];
-    strcpy(path, working_path);
-    strcat(path, "jobs/");
-    char file_name[20];
-    sprintf(file_name, "job%d.sparse", job_id-1);
-    strcat(path, file_name);
-    FILE* usage_file = fopen(path, "r+");
+GHashTable* load_historical_sparse_containers(int32_t job_id) {
+	if (job_id == 0) {
+		return NULL;
+	}
+	char path[500];
+	strcpy(path, working_path);
+	strcat(path, "jobs/");
+	char file_name[20];
+	sprintf(file_name, "job%d.sparse", job_id - 1);
+	strcat(path, file_name);
+	FILE* usage_file = fopen(path, "r+");
 
-    GHashTable* sparse_containers = g_hash_table_new_full(g_int_hash,g_int_equal, NULL, free);
+	GHashTable* sparse_containers = g_hash_table_new_full(g_int_hash,
+			g_int_equal, NULL, free);
 
-    ContainerId id;
-    int32_t size;
-    float tmp;
-    while(fscanf(usage_file, "%d:%d:%f", &id, &size, &tmp) != EOF){
-        ContainerUsage* usage = (ContainerUsage*)malloc(sizeof(ContainerUsage));
-        usage->cntnr_id = id;
-        usage->read_size = size;
-        g_hash_table_insert(sparse_containers, &usage->cntnr_id, usage);
-    }
-    fclose(usage_file);
-    return sparse_containers;
+	if (usage_file) {
+		ContainerId id;
+		int32_t size;
+		float tmp;
+		while (fscanf(usage_file, "%d:%d:%f", &id, &size, &tmp) != EOF) {
+			ContainerUsage* usage = (ContainerUsage*) malloc(
+					sizeof(ContainerUsage));
+			usage->cntnr_id = id;
+			usage->read_size = size;
+			g_hash_table_insert(sparse_containers, &usage->cntnr_id, usage);
+		}
+		fclose(usage_file);
+	}
+	return sparse_containers;
 }
 
-void destroy_historical_sparse_containers(GHashTable* sparse_containers){
-    if(sparse_containers == 0)
-        return;
-    g_hash_table_destroy(sparse_containers);
+void destroy_historical_sparse_containers(GHashTable* sparse_containers) {
+	if (sparse_containers == 0)
+		return;
+	g_hash_table_destroy(sparse_containers);
 }
