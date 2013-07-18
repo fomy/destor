e57f82dd96380f6cbbc81b886de745af84793015
fomy <fumin@hust.edu.cn>
2013-07-18 13:14:35 +0800

    scripts

--- scripts/experiments_har/rewrite/vmdk_rewrite.sh
+++ scripts/experiments_har/rewrite/vmdk_rewrite.sh
@@ -14,6 +14,9 @@ echo "index ${index} rewrite $rewrite" >>  backup.log
 jobid=0
 for file in $(ls $dataset); do
 destor $dataset/$file --index=$index --rewrite=$rewrite >>log
+if [ $jobid -ge 20 ]; then
+destor -d$(($jobid-20)) --rewrite=$rewrite --kept_versions=20
+fi
 jobid=$(($jobid+1))
 done
 
@@ -29,15 +32,15 @@ i=$(($i+1))
 done
 done
 
-for cache_size in 32 64 128 256 512 1024 2048;do
-echo "asm $cache_size" >> restore.log
-i=0
-while [ $i -lt $jobid ]
-do
-destor -r$i /home/fumin/restore/ --cache=ASM --cache_size=$cache_size >>log
-i=$(($i+1))
-done
-done
+#for cache_size in 32 64 128 256 512 1024 2048;do
+#echo "asm $cache_size" >> restore.log
+#i=0
+#while [ $i -lt $jobid ]
+#do
+#destor -r$i /home/fumin/restore/ --cache=ASM --cache_size=$cache_size >>log
+#i=$(($i+1))
+#done
+#done
 
 for cache_size in 32 64 128 256 512 1024 2048;do
 echo "opt $cache_size" >> restore.log
