cb6f2605b504914e866312b5b46b419ed861605b
fomy <fumin@hust.edu.cn>
2014-03-03 16:09:44 +0800

    caf

--- src/restore_aware.c
+++ src/restore_aware.c
@@ -19,10 +19,15 @@ struct {
 	struct lruCache *cache;
 } monitor;
 
-static int container_record_equal(struct containerRecord* a,
+static int container_record_check_id(struct containerRecord* a,
+		containerid *id){
+	return a->cid == *id ? 1 : 0;
+}
+
+/*static int container_record_equal(struct containerRecord* a,
 		struct containerRecord* b) {
 	return a->cid == b->cid ? 1 : 0;
-}
+}*/
 
 void init_restore_aware() {
 	monitor.total_size = 0;
@@ -31,7 +36,7 @@ void init_restore_aware() {
 	monitor.ocf = 0;
 	monitor.cfl = 0;
 	monitor.cache = new_lru_cache(destor.restore_cache[1], free,
-			container_record_equal);
+			container_record_check_id);
 }
 
 /*
@@ -55,9 +60,7 @@ void restore_aware_update(containerid id, int32_t chunklen) {
 }
 
 int restore_aware_contains(containerid id) {
-	struct containerRecord record;
-	record.cid = id;
-	return lru_cache_lookup_without_update(monitor.cache, &record) ? 1 : 0;
+	return lru_cache_lookup_without_update(monitor.cache, &id) ? 1 : 0;
 }
 
 double restore_aware_get_cfl() {
