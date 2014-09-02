bdb3458f4e03b9ae8eee4b2e80776447bdededf9
fomy <fumin@hust.edu.cn>
2014-09-02 09:27:22 +0800

    two new scripts for experiments

---
+++ edpl.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+dataset="kernel"
+
+if [ $# -gt 0 ];then
+    echo "dataset <- $1"
+    dataset=$1
+else
+    echo "default dataset <- $dataset"
+fi
+
+kernel_path="/home/dataset/kernel_8k/"
+vmdk_path="/home/dataset/vmdk_4k/"
+rdb_path="/home/dataset/rdb_4k/"
+synthetic_path="/home/dataset/synthetic_8k/"
+
+kernel_fcs=(16 32 64 128 256 )
+vmdk_fcs=(64 128 256 512 1024 )
+rdb_fcs=(32 64 128 256 512)
+synthetic_fcs=(32 64 128 256 512)
+
+# path: where trace files locate
+# fcs: the restore cache size
+case $dataset in
+    "kernel") 
+        path=$kernel_path
+        fcs=(${kernel_fcs[@]})
+        ;;
+    "vmdk")
+        path=$vmdk_path
+        fcs=(${vmdk_fcs[@]})
+        ;;
+    "rdb") 
+        path=$rdb_path
+        fcs=(${rdb_fcs[@]})
+        ;;
+    "synthetic") 
+        path=$synthetic_path
+        fcs=(${synthetic_fcs[@]})
+        ;;
+    *) 
+        echo "Wrong dataset!"
+        exit 1
+        ;;
+esac
+
+# ./rebuild would clear data of previous experiments
+# ./destor executes a backup job
+#   (results are written to backup.log)
+# ./destor -rN executes a restore job under various restore cache size
+#   (results are written to restore.log)
+
+param="-p\"fingerprint-index exact physical\""
+
+for s in ${fcs[@]};do
+./rebuild
+for file in $(ls $path);do
+    ./destor $path/$file $param -p"fingerprint-cache $s" >> log
+done
+./destor -s >> backup.log
+done
---
+++ ndpl.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ $# -gt 1 ];then
+    echo "dataset <- $1"
+    dataset=$1
+    echo "sampling <- $2"
+    sampling=$2
+else
+    echo "2 parameters are required"
+    exit 1
+fi
+
+kernel_path="/home/dataset/kernel_8k/"
+vmdk_path="/home/dataset/vmdk_4k/"
+rdb_path="/home/dataset/rdb_4k/"
+synthetic_path="/home/dataset/synthetic_8k/"
+
+# path: where trace files locate
+case $dataset in
+    "kernel") 
+        path=$kernel_path
+        ;;
+    "vmdk")
+        path=$vmdk_path
+        ;;
+    "rdb") 
+        path=$rdb_path
+        ;;
+    "synthetic") 
+        path=$synthetic_path
+        ;;
+    *) 
+        echo "Wrong dataset!"
+        exit 1
+        ;;
+esac
+
+# ./rebuild would clear data of previous experiments
+# ./destor executes a backup job
+#   (results are written to backup.log)
+# ./destor -rN executes a restore job under various restore cache size
+#   (results are written to restore.log)
+
+param="-p\"fingerprint-index near-exact physical\""
+
+# r is the sampling Ratio
+for r in 1 16 32 64 128 256;do
+./rebuild
+for file in $(ls $path);do
+    ./destor $path/$file $param $"fingerprint-index-sampling-method $sampling $r" >> log
+done
+./destor -s >> backup.log
+done
